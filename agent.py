81909bf0411ee4ed6b506566ec7b0217a2dfd49b
Arsallan Shahab <mdarsallan@gmail.com>
2023-12-27 20:37:40 +0530

    ✨  initial commit

---
+++ agent.py
@@ -0,0 +1,65 @@
+from uagents import Agent, Context,Model,Bureau
+from api import api
+from consts import stocks_name
+class Choice(Model):
+  choice: int
+
+class StockName(Model):
+  name: str
+
+choice_agent = Agent(name="Choice",seed="Choice Agent")
+main_agent = Agent(name="Main Agent",seed="Main Agent")
+stock_info = Agent(name="Stock Name",seed="Stock Name")
+predict_stock_price = Agent(name="Predict Stock Price",seed="Predict Stock Price")
+
+CHOICE = None
+
+@choice_agent.on_event("startup")
+async def show_choices(ctx:Context):
+  print("\n1)Show list of stocks\n2)Show a sepcified Stock\n3)Predict a Specified Stock\n4)Show recommended Stocks\n5) Exit\n")
+  CHOICE = int(input("Enter the choice: "))
+  await ctx.send(main_agent.address,Choice(choice=CHOICE))
+
+@choice_agent.on_message(model=Choice)
+async def show_choices(ctx:Context,sender,choice_agent=Choice):
+  print("1)Show list of stocks\n2)Show a sepcified Stock\n3)Predict a Specified Stock\n4)Show recommended Stocks\n5) Exit\n")
+  CHOICE = int(input("Enter the choice: "))
+  await ctx.send(main_agent.address,Choice(choice=CHOICE))
+
+@main_agent.on_message(model=Choice)
+async def choice(ctx:Context,sender,choice:Choice):
+  if(choice.choice==1):
+    for stock in  stocks_name.stock_companies:
+      print(stock["company"],stock["symbol"])
+    await ctx.send(choice_agent.address,Choice(choice=0))
+  if(choice.choice==2):
+    sk_name = input("Enter the Stock Name: ")
+    await ctx.send(stock_info.address,StockName(name=sk_name))
+  if(choice.choice==3):
+    sk_name = input("Enter the Stock Name: ")
+    await ctx.send(predict_stock_price.address,StockName(name=sk_name))
+  if(choice.choice==4):
+    api.top_gainers()
+    await ctx.send(choice_agent.address,Choice(choice=0))
+  if choice.choice == 5:
+    exit(0)
+
+
+@stock_info.on_message(model=StockName)
+async def stockName(ctx:Context,sender,stock_info:StockName):
+  api.get_stock_info(name=stock_info.name)
+  await ctx.send(choice_agent.address,Choice(choice=0))
+@predict_stock_price.on_message(model=StockName)
+async def predictStockPrice(ctx:Context,sender,stock_info:StockName):
+  api.predict_stock(name=stock_info.name)
+  await ctx.send(choice_agent.address,Choice(choice=0))
+
+breau = Bureau()
+breau.add(choice_agent)
+breau.add(main_agent)
+breau.add(stock_info)
+breau.add(predict_stock_price)
+
+if __name__ == "__main__":
+  breau.run()
+  
---
+++ api/api.py
@@ -0,0 +1,59 @@
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import pandas as pd
+import datetime
+from sklearn.metrics import mean_squared_error,r2_score
+import yfinance as yf
+
+
+def get_stock_info(name):
+    stock = yf.Ticker(name)
+    stock_history = stock.history("1yr")
+    # stock_max = stock_history["Close"].idxmax()
+    # stock_min = stock_history["Close"].idxmin()
+    print('Info')
+    print(stock.info["longName"], stock.info["longBusinessSummary"])
+    print()
+    print('ISIN')
+    print(stock.isin)
+    print()
+    print('Major Holders')
+    print(stock.major_holders)
+    print()
+    print('Institutional Holders')
+    print(stock.institutional_holders)
+  
+def predict_stock(name):
+  start = pd.to_datetime('2004-01-01')
+  stock = [name]
+  yf_initializer = yf.Ticker(name)
+  yesterday_data = yf_initializer.history("1d")
+  stock_history = yf.download(stock, start=start, end=datetime.date.today())
+  features =stock_history[['Open', 'Low', 'Close', 'Volume']]
+  target = stock_history['High']
+  X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=42)
+  model = LinearRegression()
+  model.fit(X_train, y_train)
+  # Make predictions on the test set
+  y_pred = model.predict(X_test)
+  # Evaluate the model
+  mse = mean_squared_error(y_test, y_pred)
+  r2 = r2_score(y_test, y_pred)
+  
+  X_pred = pd.DataFrame([{
+    "Open": float(yesterday_data["Open"].iloc[0]),
+    "Low": float(yesterday_data["Low"].iloc[0]),
+    "Close": float(yesterday_data["Close"].iloc[0]),
+    "Volume": float(yesterday_data["Volume"].iloc[0])
+  }])
+  y_pred =  model.predict(X_pred)
+  print(f"Today {name} high price should be: ", y_pred)
+
+def top_gainers():
+  import pandas as pd
+  gainers = pd.read_html('https://finance.yahoo.com/gainers')
+  print(gainers)
+
+  # print(f'Mean Squared Error: {mse:.2f}')
+  # print(f'R-squared: {r2:.2f}')
+  # print(f'One day data: {yesterday_data}')
